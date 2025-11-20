b5684f82402c74a2c9caea4fda50f1bf25d36afe
fard442 <mrlutton@email.neit.edu>
2025-11-20 11:07:09 -0500

    jamble

--- states/game.js
+++ states/game.js
@@ -1,7 +1,13 @@
+import { Star } from "../background.js"
 export class Game {
-
     canvas;
     pencil;
+
+    //star section
+    stars = []
+    howManyStars = 100
+
+    for(i=0, i<howManyStars, i++){}
 
     constructor(canvas, pencil){
         this.canvas = canvas;
